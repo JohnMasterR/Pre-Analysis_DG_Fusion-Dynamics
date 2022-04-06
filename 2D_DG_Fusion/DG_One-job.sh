24464d2f4d6a0a7b900162a7ea6322f2102b6a4e
JohnMasterR <72289137+JohnMasterR@users.noreply.github.com>
2022-04-06 17:34:43 -0500

    Update DG_One-job.sh

--- 2D_DG_Fusion/DG_One-job.sh
+++ 2D_DG_Fusion/DG_One-job.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# usage: DG_One-job.py N Niter Nsim Nsimst beta dt lf stf [-h] [-s] [--freediffusion] [-Nf NF] [--log LOG]
-#
+# usage: Data_Script_Generator/Dyson_Fusion_Gas.py N Niter Nsim Nsimst beta dt lf stf
+#                        				[-h] [-s] [--freediffusion] [-Nf NF] [--log LOG]
 
-#SBATCH --job-name=Test-fusion-script	#Nombre del job
-#SBATCH -p medium			#Cola a usar, Default=short (Ver colas y límites en /hpcfs/shared/README/partitions.txt)
+#SBATCH --array=0-99	#Lanza 100 Jobs numerados de 0 hasta 100
+#SBATCH --job-name=real_fusion_job		#Nombre del job
+#SBATCH -p medium		#Cola a usar, Default=short (Ver colas y límites en /hpcfs/shared/README/partitions.txt)
 #SBATCH -N 1				#Nodos requeridos, Default=1
 #SBATCH -n 1				#Tasks paralelos, recomendado para MPI, Default=1
 #SBATCH --cpus-per-task=1		#Cores requeridos por task, recomendado para multi-thread, Default=1
-#SBATCH --mem=2048			#Memoria en Mb por CPU, Default=2048
-#SBATCH --time=7-0:00:00		#Tiempo maximo de corrida, Default=2 horas
-#SBATCH -o Test-fusion-script.o%A_%a	#Nombre de archivo de salida
+#SBATCH --mem=2048		#Memoria en Mb por CPU, Default=2048
+#SBATCH --time=7-0:00:00			#Tiempo máximo de corrida, Default=2 horas
+#SBATCH -o N100_Niter100000000_Nsim10_beta0.5_dt1e-06_lf1.0/real_fusion_job.o%A_%a		#Nombre de archivo de salida
 
 # positional arguments:
 #   N                initial number of particles
@@ -32,29 +33,15 @@
 #                    detailled output of the simulation
 
 N=100
-Niter=10000000
-Nsim=1
-Nsimst=1
+Niter=100000000
+Nsim=10
+Nsimst=$(( SLURM_ARRAY_TASK_ID ))
+beta=0.5
+dt=1e-6
+lf=1.0
 stf=0
 
 module load anaconda/python3.9
-conda init bash
-source ~/bin/conda_init.sh
-conda activate cythonenv
 
-for lf in 0.1 1.0 4.0 7.0
-do
-	for beta in 0.1 1.0 3.0
-	do
-		for dt in 1e-6 1e-7 1e-8
-		do
-			inicio=`date +%s%N`
-			python3 Data_Script_Generator/Dyson_Fusion_Gas.py $N $Niter $Nsim $Nsimst $beta $dt $lf $stf &
-			fin=`date +%s%N`
-			T_time=$(($fin-$inicio))
-			printf "$lf, $beta, $dt, $T_time\n" >> Times_Niter10000000_CPU99.csv
-		done
-	done
-done
-wait
+python3 Data_Script_Generator/Dyson_Fusion_Gas.py $N $Niter $Nsim $Nsimst $beta $dt $lf $stf 
 
